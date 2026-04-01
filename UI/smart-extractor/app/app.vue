c43ed07aa96bf9627936944774adce48d95a6463
Henry-cmd325 <henrycanalesv@gmail.com>
2026-04-01 00:47:31 -0600

    fonts optimized

--- UI/smart-extractor/app/app.vue
+++ UI/smart-extractor/app/app.vue
@@ -8,9 +8,6 @@ useHead({
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' },
-    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;700;800&family=Inter:wght@400;500;600&family=Space+Grotesk:wght@400;500;700&display=swap' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap' }
   ]
 })
